c3c3bc51d16f062f038e14e1643ec4748945de10
Laziv <lazivnm@icloud.com>
2020-03-18 12:54:49 +0200

    Add lesson 10

---
+++ JavaCore/Lesson10/src/PalindromeFinder.java
@@ -0,0 +1,15 @@
+public class PalindromeFinder {
+    public static boolean isPalindrome(String word) {
+        StringBuilder newWord = new StringBuilder();
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            newWord.append(word.charAt(i));
+        }
+
+        if (word.equalsIgnoreCase(newWord.toString())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ JavaCore/Lesson10/src/TextReplacer.java
@@ -0,0 +1,8 @@
+public class TextReplacer {
+    public static String replaceAllVowelsWithDashes(String textForReplace) {
+
+        String regex = "а|я|о|у|ю|е|є|и|і|ї";
+        String replacedText = textForReplace.replaceAll(regex, "-").replaceAll(regex.toUpperCase(), "-");
+        return replacedText;
+    }
+}
